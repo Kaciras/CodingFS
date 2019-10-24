2c808de222ec32404093332e0d94ed71a1afecf9
Kaciras <Kaciras@outlook.com>
2019-10-24 13:43:53 +0800

    Add a benchmark project

---
+++ CodingFS.Benchmark/FSProxyPerf.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using BenchmarkDotNet.Attributes;
+using DokanNet;
+
+// 测试结果表明，动态代理在不出现异常的情况下比静态慢100被，出现异常的情况下慢10倍。
+// 不过即使这样，单个操作的时间仍在微秒级，影响并不大。
+namespace CodingFS.Benchmark
+{
+	public class FSProxyPerf
+	{
+		private sealed class TestFS : AbstractFileSystem
+		{
+			public override NtStatus Mounted(IDokanFileInfo _) => throw new FileNotFoundException();
+		}
+
+		private readonly IDokanOperations staticProxy = new StaticFSProxy(new TestFS());
+		private readonly IDokanOperations dynamicProxy = DynamicFSProxy.Create(new TestFS());
+
+		[Benchmark]
+		public NtStatus Static_ReadFile_Throws()
+		{
+			return staticProxy.Mounted(null);
+		}
+
+		[Benchmark]
+		public NtStatus Dynamic_ReadFile_Throws()
+		{
+			return dynamicProxy.Mounted(null);
+		}
+
+		[Benchmark]
+		public NtStatus Static_ReadFile_Success()
+		{
+			return staticProxy.FindFiles("test", out var _, null);
+		}
+
+		[Benchmark]
+		public NtStatus Dynamic_ReadFile_Success()
+		{
+			return dynamicProxy.FindFiles("test", out var _, null);
+		}
+	}
+}
---
+++ CodingFS.Benchmark/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using BenchmarkDotNet.Running;
+
+namespace CodingFS.Benchmark
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			BenchmarkRunner.Run<FSProxyPerf>();
+		}
+	}
+}
