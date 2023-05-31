53af51df9f5bae6445c53bb05bc30ea8262984c9
Kaciras <Kaciras@protonmail.com>
2023-05-31 16:41:07 +0800

    run `git gc` on repositories

--- CodingFS/Command/KeepGreen.cs
+++ CodingFS/Command/KeepGreen.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using CodingFS.Workspaces;
@@ -12,8 +13,8 @@ internal sealed class KeepGreen : CliCommand
 {
 	private RootFileClassifier filter;
 
-	int OuterDepth = 2;
-	int InnerDepth = 2;
+	int OuterDepth = 3;
+	int InnerDepth = 3;
 
 	public void Execute()
 	{
@@ -22,54 +23,84 @@ internal sealed class KeepGreen : CliCommand
 			OuterDepth = OuterDepth,
 			InnerDepth = InnerDepth,
 		};
-		Execute(filter.Root, OuterDepth, InnerDepth);
+		IterateProject(filter.Root, OuterDepth);
 	}
 
-	private void Execute(string path, int outLimit, int innerLimit)
+	TimeSpan UpdateCycle(Workspace workspace) => workspace switch
+	{
+		NpmWorkspace => TimeSpan.FromDays(7),
+		MavenWorkspace => TimeSpan.FromDays(60),
+		CargoWorkspace => TimeSpan.FromDays(30),
+		MSBuildProject w when
+		(w.SDK == MSBuildProject.SDK_CSHARP) => TimeSpan.FromDays(60),
+		_ => TimeSpan.MaxValue,
+	};
+
+	void IterateProject(string path, int outLimit)
 	{
 		var info = filter.GetWorkspaces(path);
 		var git = info.FindType<GitWorkspace>().FirstOrDefault();
 
-		if (git == null)
+		if (git != null)
 		{
-			if (--outLimit < 0)
-			{
-				return;
-			}
+			//IterateSubmodule(path, InnerDepth);
+			GitGC(git.Repository);
 		}
-		else
+
+		if (--outLimit == 0)
 		{
-			if (--innerLimit < 0)
-			{
-				return;
-			}
-			if (info.FindType<NpmWorkspace>().Any())
-			{
-				CheckGit(git.Repository, DateTimeOffset.Now.Add(TimeSpan.FromDays(-7)));
-				return;
-			}
+			return;
 		}
+
 		foreach (var entry in info.ListFiles(FileType.SourceFile))
 		{
 			if (entry is DirectoryInfo)
 			{
-				Execute(entry.FullName, outLimit, innerLimit);
+				IterateProject(entry.FullName, outLimit);
 			}
 		}
 	}
 
+	void IterateSubmodule(string path, int innerLimit)
+	{
+
+	}
+
+
 	void CheckGit(Repository repo, DateTimeOffset period)
 	{
+		var project = Path.GetDirectoryName(repo.Info.Path);
 		foreach (var commit in repo.Commits)
 		{
 			if (commit.Committer.When < period)
 			{
-				Console.WriteLine($"{repo.Info.Path} should check for update");
+				Console.WriteLine($"{project} should check for update");
 			}
 			if (commit.Message.Contains("update deps"))
 			{
 				return;
 			}
 		}
+	}
+
+	void GitGC(Repository repo)
+	{
+		var project = Path.GetDirectoryName(repo.Info.Path[..^1]);
+
+		Process.Start(new ProcessStartInfo()
+		{
+			FileName = "git",
+			Arguments = "reflog expire --all --expire=now",
+			WorkingDirectory = project,
+		})!.WaitForExit();
+
+		Process.Start(new ProcessStartInfo()
+		{
+			FileName = "git",
+			Arguments = "gc --aggressive --prune=now --quiet",
+			WorkingDirectory = project,
+		})!.WaitForExit();
+
+		Console.WriteLine($"GC completed on {project}");
 	}
 }
